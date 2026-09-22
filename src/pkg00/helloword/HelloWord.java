65e8c4c799e84d7e1f0e092c08f64aec1f08bbbd
ygoncalvesdealmeida <ygoncalvesdealmeida@350028R-F115X12.lise.kreizenn.bzh>
2026-09-22 16:37:33 +0200

    EXO 1 FAIT

--- src/pkg00/helloword/HelloWord.java
+++ src/pkg00/helloword/HelloWord.java
@@ -14,7 +14,11 @@ public class HelloWord {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        // exo 1
         System.out.println("Hello Word! by YG on github!");
+        System.out.println("Hello Word! by YG on github!2");
+        
     }
     
 }
