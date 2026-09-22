71ca7974a4cd7df254041c1c1b24a19e11d47688
ygoncalvesdealmeida <ygoncalvesdealmeida@350028R-F115X12.lise.kreizenn.bzh>
2026-09-22 17:14:08 +0200

    EXO POURSUIVIE JUSQUA 4 EN COURS

--- src/pkg00/helloword/HelloWord.java
+++ src/pkg00/helloword/HelloWord.java
@@ -1,8 +1,9 @@
-/*
+/* 
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package pkg00.helloword;
+import java.util.Scanner;
 
 /**
  *
@@ -15,10 +16,36 @@ public class HelloWord {
      */
     public static void main(String[] args) {
         
-        // exo 
+        // exo 1 
         System.out.println("Hello Word! by YG on github!");
         System.out.println("Hello Word! by YG on github!2");
         
-    }
+        // exo 2 non
+        //exo 3 
+        
+            
+            /** Scanner sc = new Scanner(System.in);
+            int monAge = 0;
+            
+            System.out.print("Quel est votre age ? : ");
+            monAge = sc.nextInt();
+            
+             System.out.println("Vous avez " + monAge + " an(s)");
+             **/ 
+            //exo 4
+            
+            Scanner sc = new Scanner(System.in);
+            int x = 0;
+            int y = 0; 
+           
+            System.out.print("quelle est votre x? : ");
+            System.out.print("quelle est votre y? : ");
+                    
+            }
+        } 
+        
+        
+        
     
-}
+    
+
